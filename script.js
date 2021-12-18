d2f83406b08d04b797913d2c363786522d6ec186
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-18 00:22:44 -0300

    Projeto Finalizado :rocket:

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ let ball = document.querySelectorAll('.ball');
 
 for(let i = 0; i < ball.length; i+=1){
     ball[i].style.backgroundColor = cores();
-    console.log(ball[i].style.backgroundColor)
 }
 
 //Texto dinâmico com nome da cor aleatório
@@ -29,27 +28,56 @@ pMensagem = document.getElementById('answer');
 pMensagem.innerText = "Escolha uma cor"
 
 
-//Verificação do resutado
+//Verificação do resutado e soma dos pontos
 for(let i = 0; i < ball.length; i+=1){
     ball[i].addEventListener('click', verificaResposta)
 }
+
+let pontos = document.getElementById('score');
+contagemPontos = 0;
+
 
 function verificaResposta(event){
     if(event.target.style.backgroundColor == rgbAleatorio.innerText){
         console.log('Acertou');
         pMensagem.innerText = 'Acertou!';
+        contagemPontos += 3;
     }else{
         pMensagem.innerText = 'Errou! Tente novamente!';
+        contagemPontos += 0; 
     }
-
+    pontos.innerText = contagemPontos;
+    salvar();
 }
-
 
 //Botão Reset
 botaoReset = document.getElementById('reset-game');
 botaoReset.addEventListener('click', reset);
 
-function reset(){
-    console.log('botão reset')
-    document.location.reload(true);
+function reset(){ 
+    //Resetando cores
+    for(let i = 0; i < ball.length; i+=1){
+        ball[i].style.backgroundColor = cores();
+    }
+   
+    //Resetando texto dinâmico 
+    window.onload = function (min, max){
+        return Math.floor(Math.random() * (max - min));
+    }
+    rgbAleatorio.innerHTML = (ball[onload(0, ball.length)].style.backgroundColor)
+    
+    //Exibindo pontos e texto interativo
+    pontos.innerText =+ localStorage.getItem('pontos')
+    pMensagem.innerText = "Escolha uma cor"
 }
+
+
+//Salva resultados
+function salvar(){
+    localStorage.setItem('pontos', pontos.innerText);
+}
+
+window.onload = function(){  
+    pontos.innerText =+ localStorage.getItem('pontos')
+}
+
